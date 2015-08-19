f0f18b05c0f2141004b441f448a48395185b6207
Devon Meunier <devon.meunier@gmail.com>
2015-08-19 10:58:49 -0400

    Add setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,4 @@ setup(
     description='Flood.io client for Python 2 and 3',
     packages=['floodio'],
     install_requires=['requests', 'python-dateutil'],
-    classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-    ]
 )
